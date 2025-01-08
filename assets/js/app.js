48362ee55e9a358a25035449ce95adaee96e3509
Ricardo Roman Landeros <ricardomon85@gmail.com>
2025-01-08 16:23:25 -0600

    subir primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+    for( let i = 2; i <= 10; i++ ){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+
+
+    for(let tipo in tipos){
+        for(let esp of especiales){
+            deck.push(esp + tipo);
+        }
+    }
+
+    deck = _.shuffle(deck);
+
+    return deck;
+}
+
+
+crearDeck()
